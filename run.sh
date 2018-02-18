89a89b81110ea1a49abfcc85f1c7bc650d2bbb80
r3dsp1 <mxkeith1904@gmail.com>
2018-02-18 23:42:15 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+sudo atiflash -p 0 asus_rx580_hynix_basic_tuned.rom
+sudo atiflash -p 1 asus_rx580_hynix_basic_tuned.rom
+sudo atiflash -p 2 asus_rx580_hynix_basic_tuned.rom
+sudo atiflash -p 3 asus_rx580_hynix_basic_tuned.rom
+sudo atiflash -p 4 asus_rx580_hynix_basic_tuned.rom
+sudo atiflash -p 5 asus_rx580_hynix_basic_tuned.rom
