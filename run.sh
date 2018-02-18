a8096f3b819981c5572e0659285c0647d8330d96
r3dsp1 <mxkeith1904@gmail.com>
2018-02-18 23:49:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ sudo atiflash -p 2 asus_rx580_hynix_basic_tuned.rom
 sudo atiflash -p 3 asus_rx580_hynix_basic_tuned.rom
 sudo atiflash -p 4 asus_rx580_hynix_basic_tuned.rom
 sudo atiflash -p 5 asus_rx580_hynix_basic_tuned.rom
+sudo reboot
