83e259cdec74624ea7d2a4edad9ebb724ede9935
r3dsp1 <mxkeith1904@gmail.com>
2018-03-18 17:27:26 +0800

    Create run_rx570.sh

---
+++ run_rx570.sh
@@ -0,0 +1,8 @@
+sudo atiflash -p 0 asus_rx570_samsung_basic_tuned.rom
+sudo atiflash -p 1 asus_rx570_samsung_basic_tuned.rom
+sudo atiflash -p 2 asus_rx570_samsung_basic_tuned.rom
+sudo atiflash -p 3 asus_rx570_samsung_basic_tuned.rom
+sudo atiflash -p 4 asus_rx570_samsung_basic_tuned.rom
+sudo atiflash -p 5 asus_rx570_samsung_basic_tuned.rom
+
+sudo reboot
