7dd361badf6771010416b85338bc9a0b0ce30e4b
r3dsp1 <mxkeith1904@gmail.com>
2018-03-18 17:48:15 +0800

    Update run_rx580.sh

--- run_rx580.sh
+++ run_rx580.sh
@@ -5,4 +5,16 @@ sudo atiflash -p 3 asus_rx580_hynix_basic_tuned.rom
 sudo atiflash -p 4 asus_rx580_hynix_basic_tuned.rom
 sudo atiflash -p 5 asus_rx580_hynix_basic_tuned.rom
 
-sudo reboot
+
+cd
+
+git clone https://github.com/r3dsp1/ethos_monitor
+
+chmod -R 775 ethos_monitor/
+
+rm -f remote.conf
+
+echo "https://configmaker.com/my/TealFlatAliveCaracal.txt" >> remote.conf
+
+putconf
+
