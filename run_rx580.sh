da5066ed03a2a4d7d06ab4d5dbadc7856672c9a7
r3dsp1 <mxkeith1904@gmail.com>
2018-03-18 18:32:25 +0800

    Update run_rx580.sh

--- run_rx580.sh
+++ run_rx580.sh
@@ -18,6 +18,6 @@ echo "https://configmaker.com/my/TealFlatAliveCaracal.txt" >> remote.conf
 
 putconf
 
-sudo datetimectl set-timezone Asia/Hong_Kong
+sudo timedatectl set-timezone Asia/Hong_Kong
 
 sudo reboot
