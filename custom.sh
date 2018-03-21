2619192ed816881dc3d1307673802ea70262f4cd
r3dsp1 <mxkeith1904@gmail.com>
2018-03-22 01:29:32 +0800

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# /home/ethos/custom.sh
+
+clear-thermals                                                                                                                                 
+./ethos_monitor/miner.py
+
+exit 0
