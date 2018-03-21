3e2cd1c3fbb137bef5aa870d0d68a90667bec6e1
r3dsp1 <mxkeith1904@gmail.com>
2018-03-22 01:27:29 +0800

    Update run_rx570.sh

--- run_rx570.sh
+++ run_rx570.sh
@@ -11,6 +11,12 @@ git clone https://github.com/r3dsp1/ethos_monitor
 
 chmod -R 775 ethos_monitor/
 
+rm -f custom.sh
+
+cp ~/fotan/custom.sh ~/custom.sh
+
+chmod -R 755 custom.sh
+
 rm -f remote.conf
 
 echo "https://configmaker.com/my/TealFlatAliveCaracal.txt" >> remote.conf
