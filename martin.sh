e00e146dc21fbf3b0b8eb2b44549b47aaadc7633
r3dsp1 <mxkeith1904@gmail.com>
2018-04-25 01:49:46 +0800

    Create martin.sh

---
+++ martin.sh
@@ -0,0 +1,22 @@
+cd
+
+sudo update-miner claymore
+
+git clone https://github.com/r3dsp1/ethos_monitor
+
+chmod -R 775 ethos_monitor/
+
+rm -f custom.sh
+
+cp ~/fotan/custom.sh ~/custom.sh
+
+chmod -R 755 custom.sh
+
+
+echo "https://configmaker.com/my/DoubleCheapBlondCougar.txt" > remote.conf
+
+putconf
+
+sudo timedatectl set-timezone Asia/Hong_Kong
+
+sudo reboot
