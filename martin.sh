6ebed20d6185033bb498de2d67069ad8c057ff73
r3dsp1 <mxkeith1904@gmail.com>
2018-06-08 01:21:20 +0800

    Update martin.sh

--- martin.sh
+++ martin.sh
@@ -1,7 +1,5 @@
 cd
 
-sudo update-miner claymore
-
 git clone https://github.com/r3dsp1/ethos_monitor
 
 chmod -R 775 ethos_monitor/
@@ -12,10 +10,7 @@ cp ~/fotan/custom.sh ~/custom.sh
 
 chmod -R 755 custom.sh
 
-
-echo "https://configmaker.com/my/DoubleCheapBlondCougar.txt" > remote.conf
-
-putconf
+echo "1" > /opt/ethos/etc/autorebooted.file
 
 sudo timedatectl set-timezone Asia/Hong_Kong
 
