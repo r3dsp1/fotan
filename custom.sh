88069cfe0bd998fdbc0c20ea40984cc12cdad23c
r3dsp1 <mxkeith1904@gmail.com>
2018-06-08 01:32:22 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # /home/ethos/custom.sh
 
-echo "1" > /opt/ethos/etc/autorebooted.file                                                                                                                                 
+# update monitor on boot
+rm -f -R ethos_monitor
+git clone https://github.com/r3dsp1/ethos_monitor
+chmod -R 775 ethos_monitor/
+
+# config file
+echo "1" > /opt/ethos/etc/autorebooted.file      
+
+# start monitor
 ./ethos_monitor/miner.py
 
 exit 0
